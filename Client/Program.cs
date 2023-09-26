9b9db3e7e6edbefb9e144e4f79bdf432c2efb39a
Imran Merchant <imerchant@outlook.com>
2023-09-25 20:46:50 -0500

    add BlazorWithIdentity template over stock blazor template

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,11 +1,20 @@
+﻿using BlazorCookieIdentity.Client;
+using BlazorCookieIdentity.Client.Services.Contracts;
+using BlazorCookieIdentity.Client.Services.Implementations;
+using BlazorCookieIdentity.Client.States;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using BlazorCookieIdentity.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddOptions();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<IdentityAuthenticationStateProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider>(s => s.GetRequiredService<IdentityAuthenticationStateProvider>());
+builder.Services.AddScoped<IAuthorizeApi, AuthorizeApi>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
